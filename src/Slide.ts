1ce1f0f2458d1757f510739948af5960fc2a2b2d
unknown <vitosdeveloper@gmail.com>
2023-04-05 10:46:51 -0300

    prev e next

--- src/Slide.ts
+++ src/Slide.ts
@@ -19,7 +19,7 @@ export default class Slide {
     this.index = 0;
     this.slide = this.slides[this.index];
 
-    this.show(this.index);
+    this.init();
   }
   hide(el: Element) {
     el.classList.remove('active');
@@ -29,5 +29,32 @@ export default class Slide {
     this.slide = this.slides[this.index];
     this.slides.forEach(this.hide);
     this.slide.classList.add('active');
+  }
+  prev() {
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+  next() {
+    const next = this.index < this.slides.length - 1 ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    prevButton.innerText = 'Slide Anterior';
+    nextButton.innerText = 'Próximo Slide';
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+    prevButton.addEventListener('pointerup', () => {
+      this.prev();
+    });
+    nextButton.addEventListener('pointerup', () => {
+      this.next();
+    });
+  }
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
