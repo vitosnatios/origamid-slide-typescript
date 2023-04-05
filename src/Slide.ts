a20f0871ff1e8f9b810236f3bceeb2fef20784be
unknown <vitosdeveloper@gmail.com>
2023-04-05 10:07:56 -0300

    show slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,18 +1,33 @@
 export default class Slide {
   container;
-  elements;
+  slides;
   controls;
   time;
+  index;
+  slide: Element;
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
-    console.log(this.container, this.elements, this.controls, this.time);
+
+    this.index = 0;
+    this.slide = this.slides[this.index];
+
+    this.show(this.index);
+  }
+  hide(el: Element) {
+    el.classList.remove('active');
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    this.slides.forEach(this.hide);
+    this.slide.classList.add('active');
   }
 }
