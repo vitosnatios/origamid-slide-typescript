7bffeca5dd0cd61419627d64cc2ca7fb46dc272c
unknown <vitosdeveloper@gmail.com>
2023-04-05 11:50:13 -0300

    timeout pause 1

--- src/Slide.ts
+++ src/Slide.ts
@@ -8,6 +8,8 @@ export default class Slide {
   index;
   slide: Element;
   timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
   constructor(
     container: Element,
     slides: Element[],
@@ -20,8 +22,10 @@ export default class Slide {
     this.time = time;
 
     this.timeout = null;
+    this.pausedTimeout = null;
     this.index = 0;
     this.slide = this.slides[this.index];
+    this.paused = false;
 
     this.init();
   }
@@ -37,19 +41,30 @@ export default class Slide {
   }
   auto(time: number) {
     this.timeout?.clear();
-    this.timeout = new Timeout(() => this.next(), this.time);
-    // const id = setTimeout(() => this.next(), time);
-    // clearTimeout();
+    this.timeout = new Timeout(() => this.next(), time);
   }
   prev() {
+    if (this.paused) return;
     const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
     this.show(prev);
   }
   next() {
+    if (this.paused) return;
     const next = this.index < this.slides.length - 1 ? this.index + 1 : 0;
     this.show(next);
   }
-
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.paused = true;
+    }, 300);
+  }
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.auto(this.time);
+    }
+  }
   private addControls() {
     const prevButton = document.createElement('button');
     const nextButton = document.createElement('button');
@@ -57,6 +72,9 @@ export default class Slide {
     nextButton.innerText = 'Próximo Slide';
     this.controls.appendChild(prevButton);
     this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener('pointerdown', () => this.pause());
+    this.controls.addEventListener('pointerup', () => this.continue());
     prevButton.addEventListener('pointerup', () => {
       this.prev();
     });
